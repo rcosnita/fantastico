674ffdb182e03486ddc64930daa37d8f06965353
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-07 21:55:48 +0300

    I fixed the run tests script so that it works as expected.

--- fantastico/run_tests.sh
+++ fantastico/run_tests.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-nosetests-3.2 --all-modules --with-xcoverage --cover-erase --cover-tests --cover-package=fantastico --xcoverage-file=fantastico_coverage.xml --with-xunit --xunit-file=fantastico_tests.xml -v
+nosetests-3.2 --all-modules --with-coverage --cover-erase --cover-tests --cover-package=fantastico --with-xunit --xunit-file=fantastico_tests.xml -v
+coverage xml
--- virtual_env/setup_dev_env.sh
+++ virtual_env/setup_dev_env.sh
@@ -6,7 +6,3 @@ virtualenv-3.2 --distribute pip-deps
 source pip-deps/bin/activate
 pip-3.2 install nose
 pip-3.2 install pylint
-
-git clone https://github.com/cmheisel/nose-xcover.git
-cd nose-xcover
-python3 setup.py install
