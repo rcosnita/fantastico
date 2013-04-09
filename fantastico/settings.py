ca473f1251d5589628be09b2b9c06620e0b5128d
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-09 15:35:00 +0300

    I correctly setup the script for generating project documentation.

---
+++ fantastico/settings.py
@@ -0,0 +1,7 @@
+'''
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+
+.. py:module:: fantastico.settings
+
+This is a simple description that must be included into sphinx generated doc.
+'''
