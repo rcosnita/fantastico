abacd1b950d63cd10357cf15e12538aa24fd4c78
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-13 11:36:45 +0300

    I added a sample unit test for settings module.

---
+++ fantastico/tests/test_settings.py
@@ -0,0 +1,13 @@
+'''
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+
+.. py:module:: fantastico.tests.test_settings
+'''
+from unittest.case import TestCase
+
+
+class TestSettingsSuite(TestCase):
+    def test_settings_ok(self):
+        '''Test case for ensuring that fantastico settings are ok.'''
+        
+        self.assertTrue(True)
