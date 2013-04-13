1d41a260caef3b1585bf14ddf8bf8cd53dd696db
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-13 11:16:00 +0300

    I improved run tests script so that it correctly generate coverage.

--- virtual_env/setup_dev_env.sh
+++ virtual_env/setup_dev_env.sh
@@ -5,6 +5,7 @@ virtualenv-3.2 --distribute pip-deps
 
 . pip-deps/bin/activate
 pip-3.2 install nose
+pip-3.2 install coverage
 pip-3.2 install pylint
 pip-3.2 install jinja2
 pip-3.2 install sphinx
