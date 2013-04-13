b5575b8717b53cf3380e3d23b8c3b66c0c329ccf
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-13 10:20:50 +0300

    I changed the virtual env setup script.

--- virtual_env/setup_dev_env.sh
+++ virtual_env/setup_dev_env.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 PATH=/usr/bin:/usr/local/bin:/usr/sbin
 
 virtualenv-3.2 --distribute pip-deps
 
-source pip-deps/bin/activate
+. pip-deps/bin/activate
 pip-3.2 install nose
 pip-3.2 install pylint
 pip-3.2 install jinja2
