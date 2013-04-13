2afc1c0bce227ccddd2c857ded8a79d7ba48501e
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-13 11:16:00 +0300

    I improved run tests script so that it correctly generate coverage.

--- fantastico/run_tests.sh
+++ fantastico/run_tests.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+. ../pip-deps/bin/activate
+
 nosetests-3.2 --all-modules --with-coverage --cover-erase --cover-tests --cover-package=fantastico --with-xunit --xunit-file=fantastico_tests.xml -v
 coverage xml
