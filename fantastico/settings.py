372ea65175845a584f36dd865ad60f94f7cf4a81
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-21 13:56:36 +0300

    I updated documentation for fantastico settings and I implemented the core settings support.

--- fantastico/settings.py
+++ fantastico/settings.py
@@ -18,6 +18,10 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 .. py:module:: fantastico.settings
 '''
 
+import os
+from fantastico.utils import instantiator
+from fantastico.exceptions import FantasticoSettingNotFoundError
+
 class BasicSettings(object):
     '''This is the core class that describes all available settings of fantastico framework. For convenience all options
     have default values that ensure minimum functionality of the framework. Below you can find an example of three possible 
@@ -33,10 +37,62 @@ class BasicSettings(object):
     def installed_middleware(self):
         '''Property that holds all installed middlewares.'''
         
-        return []
+        return ["fantastico.middleware.RequestResponseMiddleware",
+                "fantastico.middleware.RoutingEngineMiddleware"]
     
     @property
     def supported_languages(self):
         '''Property that holds all supported languages this fantastico instance.''' 
         
         return {}
+    
+class SettingsFacade(object):
+    '''For using a specific fantastico configuration you need to do two simple steps:
+
+    * Set **FANTASTICO_ACTIVE_CONFIG** to the fully python qualified class name you want to use. E.g: :py:class:`fantastico.settings.BasicSettings`
+    * In your code, you can use the following snippet to access a specific setting:
+      
+      .. code-block:: python
+      
+         from fantastico.settings import SettingsFacade
+         
+         print(SettingsFacade().get("installed_middleware"))
+                 
+    If no active configuration is set in the :py:class:`fantastico.settings.BasicSettings` will be used.'''
+    
+    __ENV_ACTIVE_CONFIG = "FANTASTICO_ACTIVE_CONFIG"
+    
+    def __init__(self, environ=None):
+        self._environ = environ
+        
+        if self._environ is None:
+            self._environ = os.environ
+            
+        self._cached_config = None
+        
+    def get(self, name):
+        '''Method used to retrieve a setting value.
+        
+        :param name: Setting name.
+        :param type: string
+        :returns: The setting value.
+        :rtype: object'''
+        
+        self._cached_config = self._cached_config or self.get_config()
+        
+        try:
+            return getattr(self._cached_config, name)
+        except AttributeError as ex:
+            raise FantasticoSettingNotFoundError("Attribute %s could not be obtained: %s" % (name, str(ex)))
+        except Exception as ex:
+            raise FantasticoSettingNotFoundError("Exception thrown for attribute %s : %s" % (name, str(ex)))
+    
+    def get_config(self):
+        '''Method used to return the active configuration which is used by this facade.
+        
+        :rtype: :py:class:`fantastico.settings.BasicSettings`
+        :returns: Active configuration currently used.'''
+        
+        active_config = self._environ.get(self.__ENV_ACTIVE_CONFIG) or "fantastico.settings.BasicSettings"
+        
+        return instantiator.instantiate_class(active_config)
