5dcd77991a8cf800fc18905bcab73899b239610f
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-22 17:49:21 +0300

    I fixed an issue with code coverage file paths.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-. ../pip-deps/bin/activate
+. pip-deps/bin/activate
 
 nosetests-3.2 --all-modules --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
