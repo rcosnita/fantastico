27b6a0b48d464f764fa21369e1d9d2a471da0444
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-24 09:39:28 +0300

    I added doc / support for request object. In addition I added request context object for future extension.

--- doc/source/features/request_response.rst
+++ doc/source/features/request_response.rst
@@ -16,3 +16,37 @@ be generated. Below, you can find a simple example of how requests are processed
 
 In order to not reinvent the wheels fantastico relies on WebOb python framework in order to correctly generate request and response
 objects. For more information read `WebOB Doc <http://docs.webob.org/en/latest/reference.html>`_.
+
+Request context
+---------------
+
+In comparison with WebOb **Fantastico** provides a nice improvement. For facilitating easy development of code, each fantastico
+request has a special attribute called context. Below you can find the attributes of a request context object:
+
+   * settings facade (:doc:`/get_started/settings`)
+   * session (not yet supported)
+   * **language** The current preferred by user. This is determined based on user lang header.
+   * user (not yet supported)
+   
+.. autoclass:: fantastico.middleware.request_context.RequestContext
+   :members:
+   
+Obtain request language
+-----------------------
+
+.. autoclass:: fantastico.locale.language.Language
+   :members:
+   
+Obtain settings using request
+-----------------------------
+
+It is recommended to use *request.context* object to obtain fantastico settings. This hides the complexity of choosing the right
+configuration and accessing attributes from it.
+
+.. code-block:: python
+
+   installed_middleware = request.context.get("installed_middleware")
+   
+   print(installed_middleware)
+
+For more information about how to configure **Fantastico** please read :doc:`/get_started/settings`.
---
+++ fantastico/locale/language.py
@@ -0,0 +1,41 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+
+.. py:module:: fantastico.locale.language
+'''
+
+class Language(object):
+    '''Class used to define how does language object looks like. There are various use cases for using language but
+    the simplest one is in request context object: 
+    
+    .. code-block:: python
+    
+       language = request.context.language
+       
+       if language.code = "en_us":
+          print("English (US) language").
+       else:
+          raise Exception("Language %s is not supported." % language.code) 
+    '''
+    
+    def __init__(self, code):
+        self.code = code
+        
+    def __str__(self):
+        '''Method used to convert the current instance into a string object.'''
+        
+        return self.code
