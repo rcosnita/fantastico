fa3b1cb46059f822d2d27e9e2bf8ffd70bd645e8
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-25 17:18:39 +0300

    I changed run tests so that it covers only unit tests.

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 . pip-deps/bin/activate
 
-nosetests-3.2 --all-modules --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
+find fantastico -regextype sed -regex '.*/test_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
