66d4f5f17e408817e016b8ef75e8599d89f6be9a
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-25 18:38:27 +0300

    I added integration tests for settings. It covers BasicSettings configuration.

--- fantastico/tests/itest_settings.py
+++ fantastico/tests/itest_settings.py
@@ -17,12 +17,32 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 
 .. py:module:: fantastico.tests.itest_settings
 '''
+from fantastico.settings import SettingsFacade, BasicSettings
+import os
 import unittest
 
 class SettingsIntegration(unittest.TestCase):
     '''Test suite that ensures current settings for various environments remain stable.'''
     
-    def test_basic_settings_ok(self):
-        '''Test case that ensures basic settings are functional.'''
+    def setUp(self):
+        self._envs = [("fantastico.settings.BasicSettings", BasicSettings)]
+        self._settings_facade = SettingsFacade() 
+    
+    def test_settings_ok(self):
+        '''Test case that ensures settings are functional for each environment available.'''
         
-        raise Exception("Fuck it")
+        old_env = os.environ.get("FANTASTICO_ACTIVE_CONFIG")
+        
+        for env, settings_cls in self._envs:
+            try:
+                os.environ["FANTASTICO_ACTIVE_CONFIG"] = env
+                
+                self.assertIsInstance(self._settings_facade.get_config(), settings_cls)
+                
+                self.assertEqual(["fantastico.middleware.RequestMiddleware", "fantastico.middleware.RoutingEngineMiddleware"], 
+                                  self._settings_facade.get("installed_middleware"))
+                
+                self.assertEqual(["en_us"], self._settings_facade.get("supported_languages"))
+            finally:
+                if old_env is not None:
+                    os.environ["FANTASTICO_ACTIVE_CONFIG"] = old_env
