b969abb3e195781f7141d95d145f441efc055dc6
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-25 18:54:26 +0300

    I changed the scripts for running unit tests / integration tests so that they generate distinct coverage files.

--- run_integration.sh
+++ run_integration.sh
@@ -2,6 +2,4 @@
 
 . pip-deps/bin/activate
 
-rm coverage.xml
-rm fantastico_tests.xml
-find fantastico -regextype sed -regex '.*/itest_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
+find fantastico -regextype sed -regex '.*/itest_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --cover-xml-file=coverage_integration.xml --with-xunit --xunit-file=fantastico_integration_tests.xml -v
