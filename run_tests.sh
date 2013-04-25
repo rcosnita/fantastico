d5c3f02d7d8f9261f3723cfd39c9c3278f4152b5
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-25 17:38:29 +0300

    I added complementary script for supporting integration tests job.

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,6 @@
 
 . pip-deps/bin/activate
 
+rm coverage.xml
+rm fantastico_tests.xml
 find fantastico -regextype sed -regex '.*/test_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
