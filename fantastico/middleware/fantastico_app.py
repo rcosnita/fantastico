b26cd5ac9d2e5b68d692fc1f9cbb97b633d36835
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-27 09:49:01 +0300

    I added the entry point for fantastico app together with unit tests. Chaining works perfectly now.

---
+++ fantastico/middleware/fantastico_app.py
@@ -0,0 +1,60 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+.. py:module:: fantastico.middleware.fantastico_app
+'''
+
+from fantastico.settings import SettingsFacade
+from fantastico.utils import instantiator
+
+class FantasticoApp(object):
+    '''This class represents the wsgi application entry point. It is designed to wrap together all configured middlewares
+    and to return an http response.'''
+
+    class OldCallableApp(object):
+        '''Class used to save __call__ method from a wsgi middleware. It is used before chaining it at startup.'''
+        
+        def __init__(self, callableObj):
+            self._callableObj = callableObj
+            
+        def __call__(self, environ, start_response):
+            '''Take the same number of arguments as any other middleware __call__ method.'''
+             
+            return self._callableObj(environ, start_response)
+    
+    def __init__(self, settings_facade=SettingsFacade):
+        self._settings_facade = settings_facade()
+        
+        self._wrap_middlewares()
+        
+    def _wrap_middlewares(self):
+        '''Method used to register all configured middlewares in the correct order.'''
+        
+        installed_middlewares = self._settings_facade.get("installed_middleware")
+        
+        curr_app_cls, curr_app_inst = FantasticoApp, FantasticoApp.OldCallableApp(self.__call__)
+        
+        for middleware_cls in reversed(installed_middlewares):            
+            middleware = instantiator.instantiate_class(middleware_cls, [curr_app_inst])
+                                    
+            curr_app_cls.__call__ = lambda inst, environ, start_response: middleware(environ, start_response)
+                                    
+            curr_app_cls, curr_app_inst = middleware.__class__, FantasticoApp.OldCallableApp(middleware.__call__)
+        
+    def __call__(self, environ, start_response):
+        '''This method is used to execute the application and all configured middlewares in the correct order.'''
+        
+        return None
---
+++ fantastico/middleware/tests/test_fantastico_app.py
@@ -0,0 +1,123 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+.. py:module:: fantastico.middleware.tests.fantastico_app
+'''
+from fantastico.middleware.fantastico_app import FantasticoApp
+from fantastico.tests.base_case import FantasticoUnitTestsCase
+from mock import Mock
+from fantastico.exceptions import FantasticoClassNotFoundError
+
+class FantasticoAppTests(FantasticoUnitTestsCase):
+    '''Class that provides the test suite for ensuring that fantastico wsgi app is working as expected.'''
+
+    def init(self):
+        self._settings_facade = Mock()
+        self._settings_facade_cls = Mock(return_value=self._settings_facade)
+        
+    def test_middlewares_wrapped(self):
+        '''Test case that ensures configured middlewares from a config profile are wrapped in the correct order.'''
+        
+        self._settings_facade.get = Mock(return_value=["fantastico.middleware.tests.test_fantastico_app.MockedMiddleware"])
+        
+        environ = {}
+
+        app = FantasticoApp(self._settings_facade_cls)        
+        
+        app(environ, Mock())
+        self.assertTrue(environ.get("test_wrapped_ok"))
+        
+    def test_middleware_wrapped_in_order(self):
+        self._settings_facade.get = Mock(return_value=["fantastico.middleware.tests.test_fantastico_app.MockedMiddleware",
+                                                       "fantastico.middleware.tests.test_fantastico_app.MockedMiddleware2",
+                                                       "fantastico.middleware.tests.test_fantastico_app.MockedMiddleware3"])
+        
+        environ = {}
+        
+        app = FantasticoApp(self._settings_facade_cls)
+
+        app(environ, Mock())
+        
+        self.assertTrue(environ.get("test_wrapped_ok"))
+        
+        chained_resp = environ.get("middlewares_responses")
+        self.assertIsNotNone(chained_resp)
+        self.assertEqual(3, len(chained_resp))
+        self.assertEqual(["middleware", "middleware2", "middleware3"], chained_resp)
+        
+    def test_wrap_no_middleware(self):
+        '''Test case that ensures the app entry point works as expected even if no middlewares are installed.'''
+        
+        self._settings_facade.get = Mock(return_value=["fantastico.middleware.tests.test_fantastico_app.MockedMiddleware",
+                                                       "fantastico.middleware.tests.test_fantastico_app.MockedMiddleware2",
+                                                       "fantastico.middleware.tests.test_fantastico_app.MockedMiddleware3"])
+        
+        environ = {}
+        
+        app = FantasticoApp(self._settings_facade_cls)
+
+        app(environ, Mock())
+        
+        self.assertTrue(True)
+        
+    def test_middleware_not_found_ex(self):
+        '''Test caes that ensures an exception is raised when an invalid middleware is specified.'''
+        
+        self._settings_facade.get = Mock(return_value=["not.found.exception"])        
+        self.assertRaises(FantasticoClassNotFoundError, FantasticoApp, *[self._settings_facade_cls])
+        
+        self._settings_facade.get = Mock(return_value=["fantastico.middleware.tests.test_fantastico_app.MockedMiddleware1099"])
+        self.assertRaises(FantasticoClassNotFoundError, FantasticoApp, *[self._settings_facade_cls])
+        
+class MockedMiddleware(object):
+    '''This is a mocked middleware used for unit testing purposes.'''
+    
+    def __init__(self, app):
+        self._app = app
+        
+    def __call__(self, environ, start_response):
+        '''Method used to provide a very simple middleware action which can be asserted against.'''
+        
+        environ["test_wrapped_ok"] = True
+        environ["middlewares_responses"] = ["middleware"]
+        
+        return self._app(environ, start_response)
+    
+class MockedMiddleware2(object):
+    '''This is a mocked middleware used for unit testing purposes.'''
+    
+    def __init__(self, app):
+        self._app = app
+        
+    def __call__(self, environ, start_response):
+        '''Method used to provide a very simple middleware action which can be asserted against.'''
+        
+        environ["middlewares_responses"].append("middleware2")
+        
+        return self._app(environ, start_response)
+    
+class MockedMiddleware3(object):
+    '''This is a mocked middleware used for unit testing purposes.'''
+    
+    def __init__(self, app):
+        self._app = app
+        
+    def __call__(self, environ, start_response):
+        '''Method used to provide a very simple middleware action which can be asserted against.'''
+        
+        environ["middlewares_responses"].append("middleware3")
+        
+        return self._app(environ, start_response)            
