3a0dd26279cf59c819c98a8d0d414fc866cceb5c
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-27 12:13:14 +0300

    I fixed the readability of the code and fixed pylint errors.

--- run_pylint.sh
+++ run_pylint.sh
@@ -2,4 +2,4 @@
 
 . pip-deps/bin/activate
 
-pylint -f parseable --ignore=tests --max-line-length=130 --disable=R0201 fantastico | tee pylint.out
+pylint -f parseable --ignore=tests --max-line-length=130 --disable=R0201,W0142 fantastico | tee pylint.out
