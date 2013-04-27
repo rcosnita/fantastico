b277be29b47e2d238c8cd1c05c18db0887339a6a
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-04-27 18:45:31 +0300

    I improved the tests execution so that coverage is calculated correctly (not only for imported modules used in tests).

--- run_integration.sh
+++ run_integration.sh
@@ -2,4 +2,4 @@
 
 . pip-deps/bin/activate
 
-find fantastico -regextype sed -regex '.*/itest_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --cover-xml-file=coverage_integration.xml --with-xunit --xunit-file=fantastico_integration_tests.xml -v
+find fantastico -regextype sed -not -regex '.*/test_.*\.py' -name '*.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --cover-xml-file=coverage_integration.xml --with-xunit --xunit-file=fantastico_integration_tests.xml -v
