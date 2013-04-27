2eddee178e2f1ea8399cba7e86e8da68856f65e0
Radu Viorel Cosnita <cosnita@adobe.com>
2013-04-27 18:45:31 +0300

    I improved the tests execution so that coverage is calculated correctly (not only for imported modules used in tests).

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@
 
 . pip-deps/bin/activate
 
-find fantastico -regextype sed -regex '.*/test_.*\.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
+find fantastico -regextype sed -not -regex '.*/itest_.*\.py' -name '*.py' | xargs nosetests-3.2 --with-coverage --cover-erase --cover-tests --cover-package=fantastico --cover-xml --with-xunit --xunit-file=fantastico_tests.xml -v
