19b2942f2da237cce3fc10d5b2eec2ea7f656cd4
Radu Viorel Cosnita <cosnita@adobe.com>
2013-05-03 16:54:36 +0300

    I added dev server module together with unit tests..

---
+++ fantastico/server/dev_server.py
@@ -0,0 +1,41 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+.. py:module:: fantastico.server.dev_server
+'''
+from fantastico.middleware.fantastico_app import FantasticoApp
+from fantastico.settings import SettingsFacade
+from wsgiref.simple_server import make_server
+
+class DevServer(object):
+    '''This class provides a very simple wsgi http server that embeds Fantastico framework into it. As developer you can use
+    it to simply test your new components.'''
+    
+    def __init__(self, settings_facade=SettingsFacade):
+        self._settings_facade = settings_facade()
+        
+        self._httpd = None
+    
+    def start(self, make_server=make_server, app=FantasticoApp):
+        '''This method starts a WSGI development server. All attributes like port, hostname and protocol are read from
+        configuration file.'''
+        
+        self.port = self._settings_facade.get("dev_server_port")
+        self.hostname = self._settings_facade.get("dev_server_host")
+        
+        self._httpd = make_server(self.hostname, self.port, app())
+        
+        self._httpd.serve_forever()
