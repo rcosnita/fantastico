355cf0258f2d1eef5e4324d4565bf60abb22088a
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-03 10:57:30 +0300

    I fixed the unit tests / integration tests for settings.

--- fantastico/tests/itest_settings.py
+++ fantastico/tests/itest_settings.py
@@ -37,7 +37,7 @@ class SettingsIntegration(FantasticoIntegrationTestCase):
             self.assertIsInstance(self._settings_facade.get_config(), settings_cls)
             
             self.assertEqual(["fantastico.middleware.request_middleware.RequestMiddleware",
-                              "fantastico.middleware.routing_engine.RoutingEngineMiddleware"], 
+                              "fantastico.middleware.routing_middleware.RoutingMiddleware"], 
                               self._settings_facade.get("installed_middleware"))
             
             self.assertEqual(["en_us"], self._settings_facade.get("supported_languages"))
