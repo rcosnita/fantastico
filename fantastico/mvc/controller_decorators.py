b83226739994934b71c3d36e8dc28f90feb2e5a4
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-06 11:32:09 +0300

    I fixed a minor formatting issue in pdf doc.

--- fantastico/mvc/controller_decorators.py
+++ fantastico/mvc/controller_decorators.py
@@ -24,11 +24,13 @@ class Controller(object):
     
     .. code-block:: python
     
-        @Controller(url="/blogs/", method="GET", models={"Blog": "fantastico.plugins.blog.models.blog.Blog"])
+        @Controller(url="/blogs/", method="GET", 
+                    models={"Blog": "fantastico.plugins.blog.models.blog.Blog"])
         def list_blogs(self, request):
             Blog = request.models.Blog
         
-            blogs = Blog.all_paged(start_record=1, end_record=0, sort_expr=[asc(Blog.create_date), desc(Blog.title)])
+            blogs = Blog.all_paged(start_record=1, end_record=0, 
+                                   sort_expr=[asc(Blog.create_date), desc(Blog.title)])
         
             return Response(blogs)
             
