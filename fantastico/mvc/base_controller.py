28c2ba45a0ce0e7211573c072ab362b03ed88ee0
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-09 18:21:14 +0300

    I added support for jinja template rendering and initial unit tests.

--- fantastico/mvc/base_controller.py
+++ fantastico/mvc/base_controller.py
@@ -18,6 +18,8 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 .. py:module:: fantastico.mvc.base_controller
 '''
 from fantastico.utils import instantiator
+from jinja2.loaders import FileSystemLoader
+from jinja2.environment import Environment
 
 class BaseController(object):
     '''This class provides common methods useful for every concrete controller. Even if no type checking is done in 
@@ -26,9 +28,41 @@ class BaseController(object):
     def __init__(self, settings_facade):
         self._settings_facade = settings_facade
         
+        self._tpl_loader = None
+        self._tpl_env = None
+    
+    def __init_jinja_context(self):
+        '''This method initialize the jinja context in order to make the controller able to render jinja2 files.'''
+        
+        views_folder = ("%s%s/views/" % (self._settings_facade.get_root_folder(), self.get_component_folder()))
+        
+        self._tpl_loader = FileSystemLoader(searchpath=views_folder)
+        self._tpl_env = Environment(loader=self._tpl_loader)
+    
     def get_component_folder(self):
         '''This method is used to retrieve the component folder name under which this controller is defined.'''
         
         root_folder = self._settings_facade.get_root_folder()
         
         return instantiator.get_component_path_data(self.__class__, root_folder)[0]
+    
+    def load_template(self, tpl_name, model_data=None):
+        '''This method is responsible for loading a template from disk and render it using the given model data.
+        
+        .. code-block:: python
+        
+            @ControllerProvider()
+            class TestController(BaseController):
+                @Controller(url="/simple/test/hello", method="GET")
+                def say_hello(self, request):
+                    return Response(self.load_template("/hello.html"))
+                    
+            The above snippet will search for **hello.html** into component folder/views/.
+        '''
+        
+        if self._tpl_env is None:
+            self.__init_jinja_context()
+        
+        model_data = model_data or {}
+        
+        return self._tpl_env.get_template(tpl_name, model_data) 
