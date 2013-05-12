09f3dc0bb16a0e42828c3f270f0824cdf1730821
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-12 22:41:29 +0300

    I removed the pylint warnings.

--- fantastico/mvc/__init__.py
+++ fantastico/mvc/__init__.py
@@ -21,16 +21,16 @@ from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-BaseModel = declarative_base()
-Session = None
+BASEMODEL = declarative_base()
+SESSION = None
 
-engine = None
+ENGINE = None
 
 def init_dm_db_engine(db_config, echo=False):
     '''Method used to configure the SQL Alchemy ORM behavior for Fantastico framework. It must be executed once per wsgi
     fantastico worker.'''
 
-    global engine, Session
+    global ENGINE, SESSION
 
     conn_props = {"drivername": db_config["drivername"],
                   "username": db_config["username"],
@@ -40,8 +40,8 @@ def init_dm_db_engine(db_config, echo=False):
                   "database": db_config["database"],
                   "query": db_config["additional_params"]}
 
-    if not engine and isinstance(conn_props, dict):
+    if not ENGINE and isinstance(conn_props, dict):
         conn_data = URL(**conn_props)
-        engine = create_engine(conn_data, echo=echo)
+        ENGINE = create_engine(conn_data, echo=echo)
 
-        Session = scoped_session(sessionmaker(bind=engine))
+        SESSION = scoped_session(sessionmaker(bind=ENGINE))
