c4273cec224782188303432c285b0d0cba8d51b7
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-14 16:35:31 +0300

    I added support for module_setup.sql scripts execution.

--- run_setup_db.sh
+++ run_setup_db.sh
@@ -2,4 +2,10 @@
 
 . pip-deps/bin/activate
 
-/usr/bin/mysql --user=root --password=$MYSQL_PASSWD -h $MYSQL_HOST < virtual_env/sql/setup_database.sql
+/usr/bin/mysql --user=root --password=$MYSQL_PASSWD -h $MYSQL_HOST -v < virtual_env/sql/setup_database.sql
+
+echo "Database Fantastico created correctly." 
+
+find . -name 'module_setup.sql' | xargs /usr/bin/mysql --user=root --password=$MYSQL_PASSWD -h $MYSQL_HOST -v
+
+echo "All module_setup.sql executed correctly."
