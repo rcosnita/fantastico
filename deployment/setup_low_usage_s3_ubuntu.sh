57d8eec1cda01a9a85b0a720c765dd31b5d9cf0d
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-21 10:19:20 +0300

    I added support into nginx config to read properties from keyboard and generate a valid config file.

--- deployment/setup_low_usage_s3_ubuntu.sh
+++ deployment/setup_low_usage_s3_ubuntu.sh
@@ -4,7 +4,7 @@ PATH=/usr/bin:/usr/sbin
 sudo apt-get install nginx -y
 
 . ../pip-deps/bin/activate
-# python3 fantastico/deployment/config_nginx.py /etc/nginx
+sudo python3 ../fantastico/deployment/config_nginx.py /etc/nginx
 
 # config s3fs
 # python3 fantastico/deploymnet/config_s3.py /etc
