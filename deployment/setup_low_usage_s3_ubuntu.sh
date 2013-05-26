a90215a847914c5f132218c705b13d4abbe64950
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-26 22:52:21 +0300

    I changed the way prod server is started. I finished the script for generating nginx configuration of fantastico project.

--- deployment/setup_low_usage_s3_ubuntu.sh
+++ deployment/setup_low_usage_s3_ubuntu.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 PATH=/usr/bin:/usr/sbin
 
-sudo apt-get install nginx -y
+echo loopless | sudo -S apt-get install nginx -y
 
 . ../pip-deps/bin/activate
-sudo python3 ../fantastico/deployment/config_nginx.py /etc/nginx
+
+export PYTHONPATH=`pwd`/../
+
+python3 ../fantastico/deployment/config_nginx.py --ipaddress 127.0.0.1 --vhost-name fantastico-framework.com --uwsgi-port 12090 --root-folder `pwd` --modules-folder /fantastico/samples >> fantastico-framework.com.conf
+
+echo ROOT_PASSWD | sudo -S mv fantastico-framework.com.conf /etc/nginx/sites-available/fantastico-framework.com.conf
+echo ROOT_PASSWD | sudo -S ln -fs /etc/nginx/sites-available/fantastico-framework.com.conf /etc/nginx/sites-enabled/fantastico-framework.com.conf
+echo ROOT_PASSWD | sudo -S service nginx restart
 
 # config s3fs
-# python3 fantastico/deploymnet/config_s3.py /etc
+# python3 fantastico/deployment/config_s3.py /etc
