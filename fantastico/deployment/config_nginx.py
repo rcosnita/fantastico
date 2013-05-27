f83a203f24bdc4cd826a2f5b7d75e2f8aaf7f319
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-27 07:51:10 +0300

    I remove pylint warnings newly added with config nginx script.

--- fantastico/deployment/config_nginx.py
+++ fantastico/deployment/config_nginx.py
@@ -36,29 +36,29 @@ class ConfigNginx(object):
         root_folder = os.path.abspath(instantiator.get_class_abslocation(BasicSettings) + "../")
         
         tpl_loader = FileSystemLoader(searchpath=root_folder)
-        self._tpl_env = Environment(loader=tpl_loader)
+        self._tpl_env = Environment(loader=tpl_loader)        
         
         self._build_args_parser()
-    
+            
     def _build_args_parser(self):
         '''This method builds the args parser required to split an array of arguments into config attributes.'''
         
-        self._args_parser.add_argument("--ipaddress", dest="_ip_address", type=str,
+        self._args_parser.add_argument("--ipaddress", dest="ip_address", type=str,
                                        help="The ip address binded to the current virtual host.",
                                        required=True)
-        self._args_parser.add_argument("--vhost-name", dest="_vhost_name", type=str,
+        self._args_parser.add_argument("--vhost-name", dest="vhost_name", type=str,
                                        help="The virtual host name for this project: e.g fantastico.org",
                                        required=True)
-        self._args_parser.add_argument("--http-port", dest="_http_port", type=int,
+        self._args_parser.add_argument("--http-port", dest="http_port", type=int,
                                        help="The http port where nginx should listen for this virtual host.",
                                        default=80)
-        self._args_parser.add_argument("--uwsgi-port", dest="_uwsgi_port", type=int,
+        self._args_parser.add_argument("--uwsgi-port", dest="uwsgi_port", type=int,
                                        help="Uwsgi port where nginx should proxy fantastico specific requests.",
                                        required=True)
-        self._args_parser.add_argument("--root-folder", dest="_root_folder", type=str,
+        self._args_parser.add_argument("--root-folder", dest="root_folder", type=str,
                                        help="Root folder of this fantastico project.",
                                        required=True)
-        self._args_parser.add_argument("--modules-folder", dest="_modules_folder", type=str,
+        self._args_parser.add_argument("--modules-folder", dest="modules_folder", type=str,
                                        help="Modules folder where this project holds fantastico custom components.",
                                        default="/")
     
@@ -66,14 +66,14 @@ class ConfigNginx(object):
         '''This method coordinate the config generation by parsing the given arguments and using them
         to generate nginx config file.'''
         
-        self._args_parser.parse_args(args, namespace=self)
+        args_namespace = self._args_parser.parse_args(args)
         
-        config_data = {"ip_address": self._ip_address,
-                       "vhost_name": self._vhost_name,
-                       "http_port": self._http_port,
-                       "uwsgi_port": self._uwsgi_port,
-                       "root_folder": self._root_folder,
-                       "modules_folder": self._modules_folder}
+        config_data = {"ip_address": args_namespace.ip_address,
+                       "vhost_name": args_namespace.vhost_name,
+                       "http_port": args_namespace.http_port,
+                       "uwsgi_port": args_namespace.uwsgi_port,
+                       "root_folder": args_namespace.root_folder,
+                       "modules_folder": args_namespace.modules_folder}
         
         config = self._tpl_env.get_template("/deployment/conf/nginx/fantastico-wsgi").render(config_data) 
         
