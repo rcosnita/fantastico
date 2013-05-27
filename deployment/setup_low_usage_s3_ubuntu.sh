cad8b63993a094600226734eb22e3095da22df76
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-27 09:22:19 +0300

    I added the integration tests for prod server. I updated setup script.

--- deployment/setup_low_usage_s3_ubuntu.sh
+++ deployment/setup_low_usage_s3_ubuntu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 PATH=/usr/bin:/usr/sbin
 
-echo loopless | sudo -S apt-get install nginx -y
+echo $ROOT_PASSWD | sudo -S apt-get install nginx -y
 
 . ../pip-deps/bin/activate
 
 export PYTHONPATH=`pwd`/../
 
-python3 ../fantastico/deployment/config_nginx.py --ipaddress 127.0.0.1 --vhost-name fantastico-framework.com --uwsgi-port 12090 --root-folder `pwd` --modules-folder /fantastico/samples >> fantastico-framework.com.conf
+python3 ../fantastico/deployment/config_nginx.py $@ >> fantastico-framework.com.conf
 
-echo ROOT_PASSWD | sudo -S mv fantastico-framework.com.conf /etc/nginx/sites-available/fantastico-framework.com.conf
-echo ROOT_PASSWD | sudo -S ln -fs /etc/nginx/sites-available/fantastico-framework.com.conf /etc/nginx/sites-enabled/fantastico-framework.com.conf
-echo ROOT_PASSWD | sudo -S service nginx restart
+echo $ROOT_PASSWD | sudo -S mv fantastico-framework.com.conf /etc/nginx/sites-available/fantastico-framework.com.conf
+echo $ROOT_PASSWD | sudo -S ln -fs /etc/nginx/sites-available/fantastico-framework.com.conf /etc/nginx/sites-enabled/fantastico-framework.com.conf
+echo $ROOT_PASSWD | sudo -S service nginx restart
 
 # config s3fs
 # python3 fantastico/deployment/config_s3.py /etc
