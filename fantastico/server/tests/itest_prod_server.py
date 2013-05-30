66dba0b68003e9231fdbfa7674202f22df5163ad
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-05-30 09:50:04 +0300

    I correctly refactored integration test so that it has predictable behavior.

--- fantastico/server/tests/itest_prod_server.py
+++ fantastico/server/tests/itest_prod_server.py
@@ -16,10 +16,13 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 .. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
 .. py:module:: fantastico.server.tests.itest_prod_server
 '''
+
 from fantastico.tests.base_case import FantasticoIntegrationTestCase
+from io import StringIO
 from subprocess import Popen
 from urllib.request import Request
 import os
+import socket
 import subprocess
 import threading
 import time
@@ -28,10 +31,23 @@ import urllib
 class ProdServerIntegration(FantasticoIntegrationTestCase):
     '''This class provides the test case for fantastico production server placed behind nginx.'''
     
+    MAX_RUN_TIMEOUT = 2
+    
     def init(self):
-        self._server_stdout = None
-        self._server_stderr = None
         self._server_proc = None
+    
+    def _is_port_opened(self, ip, port):
+        '''This method determines if a given port is opened or not.'''
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        try:
+            s.connect((ip, int(port)))
+            s.shutdown(2)
+            
+            return True
+        except:
+            return False
     
     def test_requests_handled_ok(self):
         '''This test case ensures requests are handled correctly by nginx.'''
@@ -39,25 +55,31 @@ class ProdServerIntegration(FantasticoIntegrationTestCase):
         
         def start_server():
             root_folder = os.path.realpath(os.getcwd() + "/../../../") + "/"
-            start_prod_server = ["/bin/sh", "run_prod_server.sh"]
-            
+            start_prod_server = ["%srun_prod_server.sh" % root_folder]
+
             self._server_proc = Popen(start_prod_server, cwd=root_folder, env=os.environ,
-                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             
-            self._server_stdout, self._server_stderr = self._server_proc.communicate()
-            
+            self._server_proc.communicate()
+                                
         server_thread = threading.Thread(target=start_server)
         server_thread.start()
         
-        while self._server_proc is None:
+        timeout = 0
+        
+        while not self._is_port_opened("127.0.0.1", "12090") and timeout <= ProdServerIntegration.MAX_RUN_TIMEOUT:
+            timeout += 0.2
             time.sleep(0.2)
-        
-        request = Request("http://localhost/mvc/hello-world") 
-        response = urllib.request.urlopen(request)
-        
-        self.assertEqual(200, response.code)
-        
+
         try:
-            self.server_thread.kill()
-        except:
-            pass
+            if timeout >= 2:
+                self.assertTrue(False, "Unable to run wsgi prod instance in %s seconds." % ProdServerIntegration.MAX_RUN_TIMEOUT)
+            else:
+                request = Request("http://fantastico-framework.com/mvc/hello-world") 
+                response = urllib.request.urlopen(request)
+            
+                self.assertEqual(200, response.code)
+        finally:
+            self._server_proc.terminate()
+            
+            Popen(["/usr/bin/killall", "-9", "uwsgi"], env=os.environ).communicate()
