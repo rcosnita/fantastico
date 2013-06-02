30c5f039ec1d686d16a3507ec315ffc58256789b
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-06-03 01:53:26 +0300

    I added the complete version of setup.py. I updated run_* scripts so that they also work for standalone projects.

--- virtual_env/setup_dev_env.sh
+++ virtual_env/setup_dev_env.sh
@@ -7,7 +7,7 @@ virtualenv-3.2 --distribute pip-deps
 pip-3.2 install nose
 pip-3.2 install coverage
 pip-3.2 install pylint
-pip-3.2 install jinja2
+pip-3.2 install jinja2==2.6
 pip-3.2 install sphinx
 pip-3.2 install webob
 pip-3.2 install virtual_env/libs/mysql-connector
