3a636fd4161f6eae45a045021ac676a6a1f68c65
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-06-03 00:50:39 +0300

    I automated data files recursive inclusion by using a custom method.

--- setup.py
+++ setup.py
@@ -22,6 +22,24 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 from distutils.core import setup
 import os
 
+def list_folder_recursive(virtual_folder, folder):
+    '''This method list a given folder and retrieves a new list containing a list of tuples (vfolder, [files]).'''
+    
+    curr_tuple = (virtual_folder, [])
+    ret_list = [curr_tuple]
+    
+    for filename in os.listdir(folder):
+        abspath = "%s/%s" % (folder, filename)
+        
+        if os.path.isfile(abspath):
+            curr_tuple[1].append(abspath)
+        elif os.path.isdir(abspath):
+            virtual_folder_new = "%s/%s" % (virtual_folder, filename)
+            
+            ret_list.extend(list_folder_recursive(virtual_folder_new, abspath))
+    
+    return ret_list
+
 setup(name="fantastico",
       version=os.environ.get("FANTASTICO_VERSION", "0.0.1"),
       description="Python MVC web framework with built in capabilities for developing Web Services and modular Web Applications.",
@@ -29,6 +47,7 @@ setup(name="fantastico",
       author_email="radu.cosnita@gmail.com",
       maintainer_email="radu.cosnita@gmail.com",
       url='https://github.com/rcosnita/fantastico/',
+      license="MIT",
       packages=["fantastico", "fantastico.deployment", "fantastico.deployment.tests", "fantastico.locale",
                 "fantastico.middleware", "fantastico.middleware.tests",
                 "fantastico.mvc", "fantastico.mvc.models", "fantastico.mvc.models.tests", 
@@ -38,6 +57,6 @@ setup(name="fantastico",
                 "fantastico.samples.mvc.tests", 
                 "fantastico.server", "fantastico.server.tests", 
                 "fantastico.tests", "fantastico.utils"],
-      data_files=[("deployment", ["deployment/conf/nginx/fantastico-uwsgi.ini", 
-                                  "deployment/conf/nginx/fantastico-wsgi"])],
-      scripts=["virtual_env/setup_dev_env.sh", "run_dev_server.sh", "run_prod_server.sh"])
+      data_files=[("scripts/fantastico", ["virtual_env/setup_dev_env.sh",
+                                          "run_dev_server.sh", "run_prod_server.sh"])]
+                  + list_folder_recursive("doc/fantastico", "doc/build"))
