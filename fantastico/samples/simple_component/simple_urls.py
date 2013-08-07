22709b6c74f41574cfa7b9bccc064c805adc7f84
Radu Viorel Cosnita <cosnita@adobe.com>
2013-08-07 23:47:20 +0300

    I added a test scenario for remote model plugged into local view.

---
+++ fantastico/samples/simple_component/simple_urls.py
@@ -0,0 +1,34 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+.. py:module:: fantastico.samples.simple_component.simple_urls
+'''
+from fantastico.mvc.base_controller import BaseController
+from fantastico.mvc.controller_decorators import ControllerProvider, Controller
+from webob.response import Response
+
+@ControllerProvider()
+class SampleUrlsController(BaseController):
+    '''This class provides some urls with limited functionality in order to enrich the samples from fantastico framework.'''
+
+    @Controller(url="/simple-component/foreign-component-reusage")
+    def refence_external_component(self, request):
+        '''This method showcase external component reusage with template overriding. Take a look at the template of this
+        controller.'''
+
+        content = self.load_template("/foreign_component_reusage.html")
+
+        return Response(content)
