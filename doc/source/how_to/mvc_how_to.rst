c6672254ba48c23e5a46abb492a34b39746bc0ce
Radu Viorel Cosnita <cosnita@adobe.com>
2013-08-15 01:05:41 +0300

    I fixed some minor bugs into documentation. I added the code for dynamic menus retrieval endpoint.

--- doc/source/how_to/mvc_how_to.rst
+++ doc/source/how_to/mvc_how_to.rst
@@ -11,20 +11,20 @@ Below you can find how to easily create **post** model.
 
 #. Create a new package called **blog**
 #. Create a new package called **blog.models**
-#. Create a new module called posts and paste the following code into it: 
+#. Create a new module called posts and paste the following code into it:
 
     .. code-block:: python
-    
+
         class Post(BaseModel):
             __tablename__ = "posts"
-        
+
             id = Column("id", Integer, primary_key=True)
-            blog_id = 
+            blog_id =
             title = Column("title", String(150))
             tags = Column("tags", String(150))
             created_date = Column("registered_date", DateTime(), default=datetime.now)
             content = Column("content", Text(100))
-            
+
 Now you have a fully functional post model mapped over **posts** table.
 
 Code the controller
@@ -37,25 +37,25 @@ Code the controller
 
         @ControllerProvider()
         class BlogsController(BaseController):
-            @Controller(url="/blogs/1/posts/", method="GET", 
+            @Controller(url="/blogs/(?P<blog_id>\\d{1,})/posts/$", method="GET",
                         models={"Post": "fantastico.plugins.blog.models.posts.Post"])
-            def list_blog_posts(self, request):
+            def list_blog_posts(self, request, blog_id):
                 Post = request.models.Post
-            
-                blog_id = int(request.params.id)
-            
-                posts = Post.get_records_paged(start_record=1, end_record=100,  
-                                       sort_expr=[ModelSort(Post.model_cls.created_date, ModelSort.ASC), 
+
+                blog_id = int(blog_id)
+
+                posts = Post.get_records_paged(start_record=1, end_record=100,
+                                       sort_expr=[ModelSort(Post.model_cls.created_date, ModelSort.ASC),
                                                   ModelSort(Post.title, ModelSort.DESC)],
                                        filter_expr=[ModelFilter(Post.model_cls.blog_id, blog_id, ModelFilter.EQ)])
-                                
+
                 response = Response()
-                response.text = self.load_template("/posts_listing.html", 
-                                                   {"posts": posts, 
+                response.text = self.load_template("/posts_listing.html",
+                                                   {"posts": posts,
                                                     "blog_id": blog_id})
-                
+
                 return response
-            
+
 Now you have a fully functional controller that will list first 100 posts.
 
 Code the view
@@ -65,12 +65,12 @@ Code the view
 #. Create a new view under **blog.views** called *posts_listing.html* and paste the following code into it:
 
     .. code-block:: html
-    
+
         <html>
             <head>
                 <title>List all available posts from blog {{blog_id}}</title>
             </head>
-            
+
             <body>
                 <ul>
                 {% for post in posts %}
@@ -79,7 +79,7 @@ Code the view
                 </ul>
             </body>
         </html>
-        
+
 Test your application
 ---------------------
 
---
+++ fantastico/contrib/dynamic_menu/tests/test_menu_controller.py
@@ -0,0 +1,121 @@
+'''
+Copyright 2013 Cosnita Radu Viorel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+.. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
+.. py:module:: fantastico.contrib.dynamic_menu.tests.test_menu_controller
+'''
+
+from fantastico.mvc.models.model_filter import ModelFilter
+from fantastico.tests.base_case import FantasticoUnitTestsCase
+from mock import Mock
+import json
+from fantastico.contrib.dynamic_menu.models.menus import DynamicMenu, DynamicMenuItem
+from fantastico.contrib.dynamic_menu.menu_exceptions import FantasticoMenuNotFoundException
+
+class DynamicMenuControllerTests(FantasticoUnitTestsCase):
+    '''This class provides the test cases required for making sure dynamic menu offers specified functionality.'''
+
+    _menu_contr = None
+
+    def init(self):
+        '''This method is invoked automatically in order to set common dependencies.'''
+
+        from fantastico.contrib.dynamic_menu.menu_controller import DynamicMenuController
+
+        self._menu_contr = DynamicMenuController(Mock())
+
+    def test_menu_items_retrieved_ok(self):
+        '''This test case ensures menu items are retrieved correctly for an existing menu identifier.'''
+
+        menu_id = 1
+        expected_items = [{"url": "test url", "target": "self", "title": "Simple title", "label": "Simple label"},
+                          {"url": "test url 2", "target": "self", "title 2": "Simple title 2", "label": "Simple label 2"}]
+
+        self._exec_retrieve_items_scenario(menu_id, expected_items, 2, expected_items)
+
+    def test_no_menu_items_available(self):
+        '''This test case ensures a menu without menu items retrieves a valid JSON object.'''
+
+        menu_id = 1
+        expected_items = None
+
+        self._exec_retrieve_items_scenario(menu_id, expected_items, 0, [])
+
+    def test_menu_notfound(self):
+        '''This test case ensures a concrete exception is thrown if the given menu identifier does not exist.'''
+
+        menu_id = 1500
+        menus_facade = Mock()
+
+        request = Mock()
+        request.models = Mock()
+        request.models.Menus = menus_facade
+
+        def find_by_pk(pk_value):
+            self.assertEqual(pk_value, {DynamicMenu.id: menu_id})
+
+            return None
+
+        menus_facade.find_by_pk = find_by_pk
+
+        with self.assertRaises(FantasticoMenuNotFoundException):
+            self._menu_contr.retrieve_menu_items(request, menu_id)
+
+    def _exec_retrieve_items_scenario(self, menu_id, mock_items, expected_items_len, expected_items):
+        request = self._mock_retrieve_items_request(menu_id, mock_items)
+
+        response = self._menu_contr.retrieve_menu_items(request, menu_id)
+
+        self.assertIsNotNone(response)
+        self.assertIsNotNone(response.body)
+        self.assertEqual(response.content_type, "application/json")
+
+        body = json.loads(response.body.decode())
+
+        self.assertIsNotNone(body)
+
+        items = body.get("items")
+
+        self.assertIsNotNone(items)
+        self.assertEqual(len(items), expected_items_len)
+        self.assertEqual(items, expected_items)
+
+    def _mock_retrieve_items_request(self, menu_id, expected_items):
+        '''This method is mocking all dependencies required in invoking retrieve_menu_items successfully. Mocks will return
+        expected_items input'''
+
+        request = Mock()
+        items_facade = Mock()
+
+        request.models = request
+        request.models.Items = items_facade
+
+        def get_records_paged(start_record, end_record, filter_expr):
+            '''This method is used to simulate model facade get_records_paged method.'''
+
+            self.assertEqual(start_record, 0)
+            self.assertEqual(end_record, self._menu_contr.max_items)
+            self.assertEqual(len(filter_expr), 1)
+
+            filter_expr = filter_expr[0]
+            self.assertEqual(filter_expr.column, DynamicMenuItem.menu_id)
+            self.assertEqual(filter_expr.ref_value, menu_id)
+            self.assertEqual(filter_expr.operation, ModelFilter.EQ)
+
+            return expected_items
+
+        items_facade.get_records_paged = get_records_paged
+
+        return request
