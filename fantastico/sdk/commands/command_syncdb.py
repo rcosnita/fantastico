bf181999b147caf549c4d9954405311347df3581
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-09-05 22:49:47 +0300

    I added the code for syncing database structure.

--- fantastico/sdk/commands/command_syncdb.py
+++ fantastico/sdk/commands/command_syncdb.py
@@ -16,8 +16,12 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEAL
 .. codeauthor:: Radu Viorel Cosnita <radu.cosnita@gmail.com>
 .. py:module:: fantastico.sdk.commands.command_syncdb
 '''
-from fantastico.sdk.sdk_core import SdkCommand
 from fantastico.sdk import sdk_decorators
+from fantastico.sdk.sdk_core import SdkCommand, SdkCommandArgument
+from fantastico.settings import SettingsFacade
+from fantastico.utils import instantiator
+from subprocess import call
+import os
 
 CMD_NAME = "syncdb"
 
@@ -38,4 +42,82 @@ class SdkCommandSyncDb(SdkCommand):
     consistent state.
 
     For possible examples of how to structure a component read :doc:`/features/component_model`
+
+    .. code-block:: bash
+
+        fsdk syncdb --db-command /usr/bin/mysql --comp-root samples
     '''
+
+    def __init__(self, args, cmd_factory, settings_facade_cls=SettingsFacade):
+        super(SdkCommandSyncDb, self).__init__(args, cmd_factory)
+
+        self._settings_facade = settings_facade_cls()
+
+    def get_arguments(self):
+        '''This method returns support arguments for **syncdb**:
+
+        #. -d --db-command path to mysql command.
+        #. -p --comp-root component root folder.
+        '''
+
+        return [SdkCommandArgument("-d", "--db-command", str, "Holds the location of sql command line. E.g: /usr/bin/mysql"),
+                SdkCommandArgument("-p", "--comp-root", str, "Holds the name of the folder where project components are placed.")]
+
+    def _is_file_supported(self, filename):
+        '''This method detects if the given filename is supported (module_setup.sql, create_data.sql).'''
+
+        return True
+
+    def _execute_script(self, filename, call_cmd):
+        '''This method execute a given file using the sql command line.'''
+
+        db_command = self._arguments.db_command
+        db_config = self._settings_facade.get("database_config")
+
+        cmd = "%(cmd_name)s --user %(username)s --password %(password)s -h %(host)s --database %(database)s --port %(port)s < %(script)s" % \
+                {"cmd_name": db_command,
+                 "username": db_config["username"],
+                 "password": db_config["password"],
+                 "host": db_config["host"],
+                 "port": db_config["port"],
+                 "database": db_config["database"],
+                 "script": filename}
+
+        call_cmd(cmd)
+
+    def _execute_scripts(self, scripts, call_cmd):
+        '''This method iterate over cached scripts and execute them using mysql command.'''
+
+        for mod_setup in scripts["module_setup.sql"]:
+            self._execute_script(mod_setup, call_cmd)
+
+        for create_data in scripts["create_data.sql"]:
+            self._execute_script(create_data, call_cmd)
+
+    def exec(self, os_lib=os, call_cmd=call):
+        '''This method executes module_setup.sql and create_data.sql scripts.'''
+
+        comp_folder = self._arguments.comp_root
+
+        files = {"module_setup.sql": [],
+                 "create_data.sql": []}
+
+        scan_folder = lambda folder: instantiator.scan_folder_by_criteria(folder,
+                                             file_matcher=lambda abspath, filename: self._is_file_supported(filename),
+                                             action=store_file,
+                                             os_lib=os_lib)
+
+        def store_file(location, filename):
+            '''This method stores all module_setup.sql and create_data.sql into a dictionary for future execution.'''
+
+            if os_lib.path.isdir(location):
+                return scan_folder(location)
+
+            if filename not in ["module_setup.sql", "create_data.sql"]:
+                return
+
+            files[filename].append(location)
+
+        scan_folder(comp_folder)
+
+        self._execute_scripts(files, call_cmd)
