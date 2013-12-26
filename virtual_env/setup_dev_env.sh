67a32bbbdeacc2f103c21083b254589fdd37a81c
Radu Viorel Cosnita <radu.cosnita@gmail.com>
2013-12-26 12:11:47 +0200

    I updated setup_dev_env so that it works with any installed version of pip.

--- virtual_env/setup_dev_env.sh
+++ virtual_env/setup_dev_env.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 PATH=/usr/bin:/usr/local/bin:/usr/sbin
 
-virtualenv-3.2 --distribute pip-deps
-
 . pip-deps/bin/activate
-pip-3.2 install nose
-pip-3.2 install coverage
-pip-3.2 install pylint
-pip-3.2 install jinja2==2.6
-pip-3.2 install sphinx
-pip-3.2 install webob
-pip-3.2 install virtual_env/libs/mysql-connector
-pip-3.2 install sqlalchemy
-pip-3.2 install mock
-pip-3.2 install uwsgi
-pip-3.2 install "pycrypto>=2.6"
+pip install nose
+pip install coverage
+pip install pylint
+pip install jinja2==2.6
+pip install sphinx
+pip install webob
+pip install virtual_env/libs/mysql-connector
+pip install sqlalchemy
+pip install mock
+pip install uwsgi
+pip install "pycrypto>=2.6"
